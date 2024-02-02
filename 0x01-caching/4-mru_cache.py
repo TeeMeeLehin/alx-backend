fd843e929824bdb60d3f9b4a0f4cc6255c357772
TeeMeeLehin <samemerald8@gmail.com>
2024-02-02 12:31:30 +0100

    added task

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ MRU Cache """
-from collections import OrderedDict
+from collections import OrderedDict, deque
 
 
 BaseCaching = __import__('base_caching').BaseCaching
@@ -13,20 +13,23 @@ class MRUCache(BaseCaching):
         """intialization func"""
         super().__init__()
         self.cache_data = OrderedDict()
+        self.queue = deque()
 
     def put(self, key, item):
         """ put func """
         if key is not None and item is not None:
+            if len(self.cache_data) == super().MAX_ITEMS:
+                l_key = self.queue.pop()
+                self.cache_data.pop(l_key)
+                print(f"DISCARD: {l_key}")
             self.cache_data[key] = item
-            max = super().MAX_ITEMS
-            if len(self.cache_data) > max:
-                key = list(self.cache_data.keys())[3]
-                self.cache_data.pop(key)
-                print(f"DISCARD: {key}")
+            self.queue.append(key)
 
     def get(self, key):
         """ get func """
         if key in self.cache_data:
             self.cache_data.move_to_end(key)
+            self.queue.remove(key)
+            self.queue.append(key)
             return self.cache_data.get(key)
         return None
